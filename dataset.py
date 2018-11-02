5c121d5a7dbb31cb46cb66974e11173c126b935f
Pengfei Gao <v_pega@163.com>
2018-11-02 17:59:38 +0800

    initial

---
+++ dataset.py
@@ -0,0 +1,89 @@
+import os
+import random
+import numpy as np
+import pandas as pd
+import torch
+from torch import tensor
+import pdb
+import jieba
+
+def sd2ud(labels):
+    '''
+    skewd_distribution_categories_uniform_distribution
+    '''
+    categories_set = np.unique(labels)
+    category_indices = []
+    for category in categories_set:
+        category_indices+=[[category, np.where(labels == category)[0]]]
+    min_category,min_category_indices = min(category_indices, key = lambda x:len(x[1]))
+    min_num = len(min_category_indices)
+    print(f"min category: {min_category}, num: {min_num}")
+    new_indices = np.array([])
+    for item in category_indices:
+        category,indices = item[0],item[1]
+        origin_len = len(indices)
+        prob = min_num/origin_len
+        mask = np.random.binomial(n=1,p=prob,size=origin_len)==1
+        indices = np.array([indices[i] for i in range(origin_len) if mask[i]])
+        print(f'category: {category}, origin_len: {origin_len}, new_len: {len(indices)}')
+        new_indices = np.hstack([new_indices,indices])
+    return new_indices
+
+def transform_x(x):
+    '''
+    '[1,2,3]' to [1,2,3]
+    '''
+    x = [int(v) for v in x[1:-1].split(',')]
+    return x
+
+def shuffle(*args):
+    if len(args) < 2:
+        raise Exception('args should be greater than 2')
+    item = np.stack(args, axis = 1)
+    np.random.shuffle(item)
+    return zip(*item)
+
+def padding(X):
+    max_len = len(max(X, key = lambda x: len(x)))
+    for x in X:
+        x+= [0]*(max_len-len(x))
+    return X
+
+def index_labels(labels,label_index):
+    return np.array([label_index[v] for v in labels])
+
+class DataSet:
+    def __init__(self, *args, **kwargs):
+        self.trn_ds = None
+        self.val_ds = None
+        self.test_ds = None
+        if 'label_index' in kwargs:
+            self.label_index = kwargs[label_index]
+            self.index_label = {v:k for k,v in self.label_index.item()}
+
+
+class TextDataSet(DataSet):
+    def make_batches(self, X, y, batch_size = 64, is_shuffle = True, is_drop_last = True):
+        pdb.set_trace()
+        if is_shuffle:
+            X,y = shuffle(X,y)
+        length = len(y)
+        num_of_batches = int(length/batch_size)
+        print(f'Making batches... batch_size: {batch_size},num of batchese: {num_of_batches}')
+        start,end,ret_X,ret_y = 0,batch_size,[],[]
+
+        for i in range(num_of_batches):
+            ret_X = X[start:end]
+            ret_y = y[start:end]
+            padding(ret_X)
+            yield tensor(ret_X),tensor(ret_y)
+            start+=batch_size
+            end+=batch_size
+        
+        if not is_drop_last and length%batch_size:
+            yield tensor(X[start:],y[start:])
+    
+#class ImageDataSet(DataSet):
+    #def classfication_from_path(self, path, bs=64, trn_name = 'train', val_name = 'valid', test_name = 'test'):
+
+
---
+++ tests/dataset_test.py
@@ -0,0 +1,39 @@
+import sys
+import os
+sys.path.append(os.path.pardir)
+import dataset as ds
+import pandas as pd
+import numpy as np
+import pdb
+
+
+df = pd.read_csv('dataset_test.csv')
+def test_sd2ud():
+    print(ds.sd2ud(df.y1)[:10])
+    print(ds.sd2ud([1,2,3,1,1,1,1,1,1,1,1,1])[:10])
+
+def test_shuffle():
+    shuffle = ds.shuffle
+    a1 = np.array([0,1,2])
+    a2 = np.array([3,4,5])
+    a3 = np.array([6,7,8])
+    x,y = shuffle(a1,a2)
+    print(x,y)
+    x,y,z = shuffle(a1,a2,a3)
+    print(x,y,z)
+
+def test_padding():
+    padding = ds.padding
+    x = [[1,2,3],[2,3],[1,2,3,4]]
+    print(padding(x)) 
+
+def test_TextDataSet():
+    pdb.set_trace()
+    label_index = {'办公费':0,'业务招待费':1,'福利费':2,'差旅费':3}
+    tds = ds.TextDataSet()
+    x = df.x.apply(ds.transform_x)
+    y = ds.index_labels(df.y1,label_index)
+    tds.make_batches(x,y)
+
+test_TextDataSet()
+    
